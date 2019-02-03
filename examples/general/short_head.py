96b7059407d8a1f6e983d067856e00e2791bfe73
Arthur-Milchior <arthur@milchior.fr>
2019-02-03 16:51:16 +0100

    head change

--- examples/general/short_head.py
+++ examples/general/short_head.py
@@ -2,5 +2,7 @@ from ...generators import *
 contextOrDeck = [FilledOrEmpty('Context',
                                DecoratedField('Context', isMandatory = True),
                                ["Deck: ",deck]), hr]
-short_header =CLASS("head",[contextOrDeck,('Variables', [DecoratedField('Variables'), hr] ),('Assuming',[DecoratedField('Assuming'),br])])
+def df(name):
+    return DecoratedField(name,suffix=hr)
 
+short_header =CLASS("head",[contextOrDeck,df('Variables'),df('Assuming'),df('Intuition')] )
