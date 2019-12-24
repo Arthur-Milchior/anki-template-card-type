63440d97e431ea7830d9301ec43b9c14100dda96
Arthur Milchior <Arthur@Milchior.fr>
2019-12-24 01:57:25 -0800

    add song

---
+++ examples/song.py
@@ -0,0 +1,49 @@
+from ..generators import *
+from .general.foot import footer
+from .general.head import header
+from .general.typ import typDic
+
+def toField(n):
+    if n==1:
+        return "vers"
+    else:
+        return f"vers{n}"
+
+def toPart(n):
+    if n==1:
+        return "Part"
+    else:
+        return f"Part{n}"
+
+def grouping(subgroup, nb=0, size=10, part="Part"):
+    partName = f"{part}{nb+1}" if nb else part
+    verses = [toField(i) for i in range(size*nb+1, size*(nb+1)+1)]
+    prefix = AtLeastOneField(child=hr, fields=verses)
+    verses = {*verses}
+    subgroup = Cascade(partName, subgroup, verses)
+    return [prefix, subgroup]
+
+prefix = TableFields(['Titre', 'Interprète', 'Auteur'])
+def song(nb, nbQuestions):
+    lyrics = NumberedFields(fieldPrefix="vers",
+                            label="",
+                            greater=nb,
+                            applyToGroup=grouping,
+                            groupSize=10,
+                            globalFun=identity)
+    if nbQuestions == 1:
+        questions = frozenset({toField(nb)})
+    elif nbQuestions == 2:
+        questions = frozenset({toField(nb), toField(nb-1)})
+    else:
+        assert nbQuestions % 10 ==0
+        nbParts = nbQuestions / 10
+        if nbParts == 1:
+            questions = frozenset({toPart(nb/10)})
+        elif nbParts == 2:
+            questions = frozenset({toPart(nb/10), toPart(nb/10-1)})
+        else:
+            assert False
+    print(f"questions is {questions}")
+    lyrics = lyrics.getNormalForm().assumeAsked(questions)
+    return [prefix, lyrics]
